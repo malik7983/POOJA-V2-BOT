61ceb7cd53eed588c0ce1c5cc6d8396d8bda2d9e
Aswin <100590136+Botowner@users.noreply.github.com>
2022-07-29 17:45:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Botowner/Botowner.git /Botowner
+  git clone https://github.com/Botowner/Elsa-V3.git /Elsa-V3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Botowner
+  git clone $UPSTREAM_REPO /Elsa-V3
 fi
-cd /Botowner
+cd /Elsa-V3
 pip3 install -U -r requirements.txt
-echo "Starting 𝘋𝘒_𝘉𝘖𝘛𝘹...."
+echo "Starting Elsa-V3....❄️"
 python3 bot.py
