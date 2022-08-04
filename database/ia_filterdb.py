817159523c05b4a5e523df7a1b8c3748ac9ca37e
Aswin <100590136+Botowner@users.noreply.github.com>
2022-08-04 18:30:50 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -5,7 +5,7 @@ from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
-from info import DATABASE_URI, DATABASE_NAME, temp
+from info import DATABASE_URI, DATABASE_NAME
 from utils import temp
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
