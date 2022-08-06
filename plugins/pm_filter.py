5f58bba821484948d7a09c691097ac170153fe8e
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-08-06 23:39:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1096,11 +1096,11 @@ async def advantage_spell_chok(msg):
         return
     SPELL_CHECK[msg.id] = movielist
     # btn = [InlineKeyboardButton("🔍ɢᴏᴏɢʟᴇ🔎", url=f'https://google.com/search?q={query}')]
-    await msg.reply(
+    await msg.reply_text(
         text="𝖡𝗋𝗈, 𝖢𝗁𝖾𝖼𝗄 𝗍𝗁𝖾 𝗌𝗉𝖾𝗅𝗅𝗂𝗇𝗀 𝖸𝗈𝗎 𝗁𝖺𝗏𝖾 𝗌𝖾𝗇𝖽 𝗂𝗇 𝗀𝗈𝗈𝗀𝗅𝖾. 𝖨𝖿 𝖸𝗈𝗎 𝗁𝖺𝗏𝖾 𝗋𝖾𝗊𝗎𝖾𝗌𝗍𝖾𝖽 𝖥𝗈𝗋 𝖢𝖺𝗆 𝗉𝗋𝗂𝗇𝗍 𝖸𝗈𝗎 𝗐𝗂𝗅𝗅 𝗇𝗈𝗍 𝖦𝖾𝗍 𝗂𝗍.", 
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton("🔍ɢᴏᴏɢʟᴇ🔎", url=f'https://google.com/search?q={query}')
+                InlineKeyboardButton("🔍ɢᴏᴏɢʟᴇ🔎", url=f"https://google.com/search?q={query}")
             ]]
         )
     )
