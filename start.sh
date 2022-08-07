b6f087c103bd7ff10f79e416d62b9c7419076598
BC Maniac <98605163+BC-Maniac@users.noreply.github.com>
2022-08-07 16:18:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Botowner/Elsa-V3.git /Elsa-V3
+  git clone https://github.com/BC-Maniac/POOJA-V2-BOT.git /POOJA-V2-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Elsa-V3
+  git clone $UPSTREAM_REPO /POOJA-V2-BOT
 fi
 cd /Elsa-V3
 pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖-𝙑𝟑....🧞‍♂️"
+echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 Pooja....🧞‍♂️"
 python3 bot.py
